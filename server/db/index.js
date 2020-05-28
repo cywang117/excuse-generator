0f753e22947b27ffcadb0a02cee2d850a1cbc10e
Christina Wang <cywang117@outlook.com>
2020-05-28 22:14:02 +0900

    Add like/unlike feature, add sessions to track

--- server/db/index.js
+++ server/db/index.js
@@ -5,14 +5,31 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/excuseWizard', { 
 
 let excuseSchema = new mongoose.Schema({
   excuse: String,
-  likes: Number
+  likes: { type: Number, default: 1 }
 });
 
 const Excuse = mongoose.model('Excuse', excuseSchema);
 
-exports.listAll = async () => {
-  let results = await Excuse.find();
+exports.listTopN = async (n = 10) => {
+  let results = await Excuse.find().sort({ likes: -1 }).limit(n).exec();
   return results;
+};
+
+exports.incrementLikes = async ({ _id }) => {
+  Excuse.updateOne(
+    { _id },
+    { $inc: { likes: 1 }},
+    { upsert: true }
+  )
+    .catch(err => { throw err; });
+};
+
+exports.decrementLikes = async ({ _id }) => {
+  Excuse.updateOne(
+    { _id },
+    { $inc: { likes: -1 }}
+  )
+    .catch(err => { throw err; });
 };
 
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const cookieSession = require('cookie-session');
 const express = require('express');
 const app = express();
 const router = express.Router();
 
-const { listAll } = require('./db/index');
+const { listTopN, incrementLikes, decrementLikes } = require('./db/index');
 const { excuseMarkovChain } = require('./utils/markovChain');
+
+app.use(cookieSession({
+  name: 'excuseWizard',
+  secret: 'abracadabra',
+  maxAge: 24 * 60 * 60 * 1000,
+}));
 
 app.use(express.json());
 app.use('/api', router);
@@ -18,6 +25,51 @@ router.get('/excuse', async (req, res) => {
   } catch(e) {
     console.error(e);
     res.sendStatus(500);
+  }
+});
+
+router.get('/likes', async (req, res) => {
+  try {
+    let top10 = await listTopN(10);
+    res.status(200).json(top10);
+  } catch(e) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/likes/session', (req, res) => {
+  if (!req.session.likes) {
+    req.session.likes = {};
+  }
+  res.status(200).json({ ...req.session.likes });
+});
+
+router.post('/like', async (req, res) => {
+  let { _id } = req.body;
+  if (!req.session.likes) {
+    req.session.likes = {};
+  }
+  if (!req.session.likes[_id]) {
+    req.session.likes[_id] = true;
+    await incrementLikes({ _id  });
+    res.status(201).json({ message: 'Post successfully liked' });
+  } else {
+    res.status(400).json({ error: 'Post is already liked' });
+  }
+
+});
+
+router.post('/unlike', async (req, res) => {
+  let { _id } = req.body;
+  if (!req.session.likes) {
+    req.session.likes = {};
+  }
+  if (req.session.likes[_id ]) {
+    delete req.session.likes[_id ];
+    await decrementLikes({ _id  });
+    res.status(200).json({ message: 'Post successfully unliked' });
+  } else {
+    res.status(400).json({ error: 'Post is already unliked' });
   }
 });
 
