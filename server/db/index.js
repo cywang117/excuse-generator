6ff7107d18f556c0644eaf288b6960642d9d97e8
Christina Wang <cywang117@outlook.com>
2020-05-29 17:35:55 +0900

    Clean up post route logic, add better error handling

--- server/db/index.js
+++ server/db/index.js
@@ -5,17 +5,21 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/excuseWizard', { 
 
 let excuseSchema = new mongoose.Schema({
   excuse: String,
-  likes: { type: Number, default: 1 }
-});
+  likes: {
+    type: Number,
+    default: 1,
+    min: 0
+  }
+}, { timestamps: true });
 
 const Excuse = mongoose.model('Excuse', excuseSchema);
 
 exports.listTopN = async (n = 10) => {
-  let results = await Excuse.find().sort({ likes: -1 }).limit(n).exec();
+  let results = await Excuse.find().sort({ likes: -1, updatedAt: -1 }).limit(n).exec();
   return results;
 };
 
-exports.incrementLikes = async ({ _id, excuse }) => {
+exports.incrementLikes = async (_id, excuse) => {
   return Excuse.updateOne(
     { excuse },
     { $inc: { likes: 1 }},
@@ -25,17 +29,25 @@ exports.incrementLikes = async ({ _id, excuse }) => {
       if (res.upserted) {
         return res.upserted[0]._id;
       } else {
-        return null;
+        return _id;
       }
     })
     .catch(err => { throw err; });
 };
 
-exports.decrementLikes = async ({ _id }) => {
-  Excuse.updateOne(
-    { _id },
+exports.decrementLikes = async (_id, excuse) => {
+  return Excuse.updateOne(
+    { excuse },
     { $inc: { likes: -1 }}
   )
+    .then(res => res.ok ? _id : null)
+    .catch(err => { throw err; });
+};
+
+exports.getExcuseId = async (excuse) => {
+  return Excuse.findOne({ excuse })
+    .exec()
+    .then(res => res ? res._id : null)
     .catch(err => { throw err; });
 };
 
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-const { listTopN, incrementLikes, decrementLikes } = require('./db/index');
+const { listTopN, incrementLikes, decrementLikes, getExcuseId } = require('./db/index');
 const { excuseMarkovChain } = require('./utils/markovChain');
 
 app.use(cookieSession({
@@ -15,7 +15,7 @@ app.use(cookieSession({
 
 app.use(express.json());
 app.use('/api', router);
-app.use(express.static(path.resolve(__dirname, '..', 'client')));
+app.use(express.static(path.resolve(__dirname, '..', 'client', 'dist')));
 
 router.get('/excuse', async (req, res) => {
   try {
@@ -33,6 +33,7 @@ router.get('/likes', async (req, res) => {
     let top10 = await listTopN(10);
     res.status(200).json(top10);
   } catch(e) {
+    console.error(e);
     res.sendStatus(500);
   }
 });
@@ -45,35 +46,65 @@ router.get('/likes/session', (req, res) => {
 });
 
 router.post('/like', async (req, res) => {
-  let { _id, excuse } = req.body;
+  let { excuse } = req.body;
+
   if (!req.session.likes) {
     req.session.likes = {};
   }
-  if (!req.session.likes[_id]) {
-    let newIdIfNotExists = await incrementLikes({ excuse });
-    if (newIdIfNotExists) {
-      req.session.likes[newIdIfNotExists] = true;
-    } else {
-      req.session.likes[_id] = true;
-    }
-    res.status(201).json({ message: 'Post successfully liked' });
+
+  let _id;
+  try {
+    _id = await getExcuseId(excuse);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+
+  if (!_id || (_id && !req.session.likes[_id])) {
+    _id = await incrementLikes(_id, excuse);
+    req.session.likes[_id] = true;
+    res.status(201).json({ message: 'Post successfully liked', _id });
   } else {
     res.status(400).json({ error: 'Post is already liked' });
   }
-
 });
 
 router.post('/unlike', async (req, res) => {
-  let { _id } = req.body;
+  let { excuse } = req.body;
+
   if (!req.session.likes) {
     req.session.likes = {};
   }
-  if (req.session.likes[_id ]) {
-    delete req.session.likes[_id ];
-    await decrementLikes({ _id  });
-    res.status(200).json({ message: 'Post successfully unliked' });
-  } else {
+
+  let _id;
+  try {
+    _id = await getExcuseId(excuse);
+  } catch(e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+
+  if (!_id) {
+    res.status(400).json({ error: 'Post does not exist' });
+    return;
+  } else if (!req.session.likes[_id]) {
     res.status(400).json({ error: 'Post is already unliked' });
+    return;
+  }
+
+  try {
+    _id = await decrementLikes(_id, excuse);
+  } catch(e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
+
+  if (_id) {
+    delete req.session.likes[_id];
+    res.status(200).json({ message: 'Post successfully unliked', _id });
+  } else {
+    console.log('Really hope you don\'t get here');
+    res.sendStatus(500);
   }
 });
 
